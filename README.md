c3308ac7638e0413487d0ed90d088188cffb1224
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-11-26 11:33:26 +0000

    Added README.md

---
+++ README.md
@@ -0,0 +1,34 @@
+# NdT3Links
+
+This is a simple URL shortener made by NdT3Development using NodeJS and Sqlite3 as the database.
+
+## How it Works
+
+| URL Path | Method | Description|
+|    ---   |  ---   |  ---  |
+| /:id     | GET    | Gets the redirect ID from the database and redirects the user to that URL                                                                                               |
+| /new     | POST   | Creates a new database entry for a short URL. The request must contain a 'token' field with the correct token and a 'url' field with the URL that needs to be shortened |
+
+## Configuration
+
+A `config.js` file is used to configure this software. Below is an example configuration file
+
+```js
+var config = {
+  length: 6,
+  dbFile: './db.sqlite3',
+  expressProxy: '127.0.0.1',
+  morganLogFormat: 'combined',
+  token: 'tokenhere'
+};
+
+module.exports = config;
+```
+
+| Name   | Description | Value Type |
+|   ---  |     ---     |     ---    |
+| length | The length of the generated ID | Number |
+| dbFile | The path to the Sqlite3 database file | String |
+| expressProxy | The express trust proxy configuration. More info at https://expressjs.com/en/guide/behind-proxies.html | See info link: https://expressjs.com/en/guide/behind-proxies.html |
+| morganLogFormat | The format to use for logging. More info at https://www.npmjs.com/package/morgan#predefined-formats | String |
+| token | The token used to add new short IDs | String |
---
+++ server.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var bodyParser  = require('body-parser');
+var crypto = require('crypto');
+var url = require('url');
+var morgan = require('morgan');
+var app = express();
+
+try {
+  var config = require('./config.js');
+} catch (e) {
+  console.error(`Unable to load config file. Error code: ${e.code}`);
+  process.exit(1);
+}
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.set('trust proxy', config.expressProxy);
+
+app.use(morgan(config.morganLogFormat));
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(config.dbFile);
+
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: config.dbFile
+  },
+    useNullAsDefault: true
+});
+
+var cTable = knex.schema.createTableIfNotExists('links', function (table) {
+  table.string('id');
+  table.string('url');
+}).toString();
+
+//console.log(cTable);
+db.serialize(function() {
+  db.run(cTable);
+});
+
+app.get('/:id', function (req, res) {
+  if (req.params.id === 'favicon.ico' || req.params.id === 'robots.txt') return;
+  //console.log(req.params.id);
+  var selectURL = knex.select('url').from('links').where('id', req.params.id).toString();
+  //console.log(selectURL);
+  db.serialize(function() {
+    db.all(selectURL, function (err, rows) {
+      if(err){
+          console.log(err);
+      } else {
+        url = rows[0];
+        //console.log(url);
+        if (url) {
+          //console.log(url.url);
+          res.redirect(url.url);
+        } else {
+          res.status(400).json({ success: false, error: 'ID not found in database' });
+        }
+      };
+    });
+  });
+  //res.end();
+});
+
+app.post('/new', function (req, res) {
+ //console.log(req.body.pass);
+  
+  if (!req.body.token) {
+    return res.status(403).json({ success: false, error: 'No token provided' });
+  }
+  if (req.body.token !== config.token) {
+    return res.status(401).json({ success: false, error: 'Token incorrect' });
+  }
+  //console.log(req.body.url);
+  if (!req.body.url) {
+    return res.status(400).json({ success: false, error: 'URL required' });
+  }  
+    var regex = new RegExp("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
+    //console.log(regex.test(req.body.url));
+    
+    if (!regex.test(req.body.url)) {
+      return res.status(400).json({ success: false, error: 'Not a valid URL' });
+    }
+
+  
+    function randomValueHex (length) {
+      return crypto.randomBytes(Math.ceil(length/2))
+          .toString('hex') // convert to hexadecimal format
+          .slice(0,length);   // return required number of characters
+    }
+    var random = randomValueHex(config.length);
+    //console.log(random);
+
+    function insert() {
+      var q = knex('links').insert({url: req.body.url, id: random}).toString();
+      //console.log(q);
+      db.serialize(function() {
+        db.run(q, function (err) {
+          if (err) {
+            res.status(500).json({ success: false, error: 'Error in inserting values to database' });
+            return console.log(err);
+          }
+          
+          console.log(`Inserted ID ${random} into database`);
+          res.json({ success: true, URL: req.body.url, ID: random });
+        });
+      });
+    }
+  
+  
+    var check = knex.select('url').from('links').where('id', random).toString();
+    //console.log(check);
+      db.serialize(function() {
+        db.all(check, function (err, rows) {
+          if(err){
+            res.json({ success: false, error: 'Error in checking for duplicate ID' });
+            console.log(err);
+          } else {
+            name = rows[0];
+            //console.log(name);
+            if (!name) {
+              insert();
+            }
+          };
+        });
+      });
+});
+
+app.get('*', function (req, res) {
+  res.status(404).json({ success: false, error: '404 Not Found' });
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('App is listening on port ' + listener.address().port);
+});
