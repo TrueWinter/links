0cc800071fa06cb35e7adf4ba9a7cb273d64d6b0
NdT3Development <nicholis@ndt3.me>
2020-11-11 15:32:16 +0200

    stuff and things

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# NdT3Links
+# Links
 
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/93d38fa6f2e1464ba59aa001882210b3)](https://www.codacy.com/app/NdT3Development/NdT3Links?utm_source=github.com&utm_medium=referral&utm_content=NdT3Development/NdT3Links&utm_campaign=badger)
-[![Known Vulnerabilities](https://snyk.io/test/github/ndt3development/ndt3links/badge.svg)](https://snyk.io/test/github/ndt3development/ndt3links)
-
-This is a simple URL shortener made by NdT3Development using NodeJS and Sqlite3 as the database.
-
-## How it Works
-
-| URL Path | Method | Description|
-|    ---   |  ---   |  ---  |
-| /:id     | GET    | Gets the redirect ID from the database and redirects the user to that URL                                                                                               |
-| /new     | POST   | Creates a new database entry for a short URL. The request must contain a 'token' field with the correct token and a 'url' field with the URL that needs to be shortened |
+This is a simple URL shortener made by TrueWinter using NodeJS and Sqlite3 as the database.
 
 ## Configuration
 
@@ -21,17 +11,8 @@ var config = {
   length: 6,
   dbFile: './db.sqlite3',
   expressProxy: '127.0.0.1',
-  morganLogFormat: 'combined',
-  token: 'tokenhere'
+  password: 'password'
 };
 
 module.exports = config;
 ```
-
-| Name   | Description | Value Type |
-|   ---  |     ---     |     ---    |
-| length | The length of the generated ID | Number |
-| dbFile | The path to the Sqlite3 database file | String |
-| expressProxy | The express trust proxy configuration. More info at https://expressjs.com/en/guide/behind-proxies.html | See info link: https://expressjs.com/en/guide/behind-proxies.html |
-| morganLogFormat | The format to use for logging. More info at https://www.npmjs.com/package/morgan#predefined-formats | String |
-| token | The token used to add new short IDs | String |
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
 var url = require('url');
+var path = require('path');
 var morgan = require('morgan');
 var app = express();
 
@@ -21,7 +22,7 @@ app.use(bodyParser.json());
 
 app.set('trust proxy', config.expressProxy);
 
-app.use(morgan(config.morganLogFormat));
+app.use(morgan('combined'));
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(config.dbFile);
@@ -34,68 +35,67 @@ var knex = require('knex')({
 	useNullAsDefault: true
 });
 
-var cTable = knex.schema.createTableIfNotExists('links', function (table) {
-	table.string('id');
-	table.string('url');
-}).toString();
-
-//console.log(cTable);
-db.serialize(function() {
-	db.run(cTable);
+knex.schema.hasTable('links').then(function(exists) {
+	if (!exists) {
+		knex.schema.createTable('links', function(table) {
+			table.increments('id');
+			table.string('shortid');
+			table.string('url');
+			table.integer('clicks');
+		}).then(function() {
+			console.log('Table created');
+		}).catch(function(e) {
+			throw new Error(`Unable to create table: ${e}`);
+		});
+	}
+}).catch(function(e) {
+	throw new Error(`Unable to create table: ${e}`);
 });
 
-app.get('/:id', function (req, res) {
-	if (req.params.id === 'favicon.ico' || req.params.id === 'robots.txt') return res.status(404).end();
-	//console.log(req.params.id);
-	var selectURL = knex.select('url')
-		.from('links')
-		.where('id', req.params.id)
-		.toString();
-	//console.log(selectURL);
-	db.serialize(function() {
-		db.all(selectURL, function (err, rows) {
-			if (err) {
-				console.log(err);
-			} else {
-				url = rows[0];
-				if (url) {
-					res.redirect(url.url);
-				} else {
-					res.status(400).json({ success: false, error: 'ID not found in database' });
-				}
-			}
-		});
-	});
+app.get('/new', function(req, res) {
+	res.sendFile(path.join(__dirname, 'new.html'));
 });
 
 app.post('/new', function (req, res) {
 
-	if (!req.body.token) {
-		return res.status(403).json({ success: false, error: 'No token provided' });
+	if (!req.body.password) {
+		return res.status(403).end('No token provided');
 	}
-	if (req.body.token !== config.token) {
-		return res.status(401).json({ success: false, error: 'Token incorrect' });
+	if (req.body.password !== config.password) {
+		return res.status(401).end('Password incorrect');
 	}
 	//console.log(req.body.url);
 	if (!req.body.url) {
 		return res.status(400).json({ success: false, error: 'URL required' });
 	}
+
 	var regex = new RegExp('^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?'); // eslint-disable-line no-useless-escape
 
 	if (!regex.test(req.body.url)) {
-		return res.status(400).json({ success: false, error: 'Not a valid URL' });
+		return res.status(400).end('Not a valid URL');
 	}
 
-
-	function randomValueHex (length) {
+	function randomValueHex () {
+		var length = config.length;
 		return crypto.randomBytes(Math.ceil(length / 2))
 			.toString('hex') // convert to hexadecimal format
 			.slice(0, length);	 // return required number of characters
 	}
-	var random = randomValueHex(config.length);
 
-	function insert() {
-		var q = knex('links').insert({ url: req.body.url, id: random }).toString();
+	var random = randomValueHex();
+
+	knex('links').select('shortid').where({
+		id: req.body.shortid ? req.body.shortid : random
+	}).then(function(data) {
+		if (data.length !== 0) {
+			if (req.body.shortid) {
+				return res.status(500).end('Short ID is not unique');
+			}
+			return res.status(500).end('Failed to generate unique short ID');
+		}
+	});
+
+		/*var q = knex('links').insert({ url: req.body.url, id: random }).toString();
 		//console.log(q);
 		db.serialize(function() {
 			db.run(q, function (err) {
@@ -107,10 +107,20 @@ app.post('/new', function (req, res) {
 				console.log(`Inserted ID ${random} into database`);
 				res.json({ success: true, URL: req.body.url, ID: random });
 			});
-		});
-	}
+		});*/
 
-	var check = knex.select('url')
+	knex('links').insert({
+		shortid: req.body.shortid ? req.body.shortid : random,
+		url: req.body.url,
+		clicks: 0
+	}).then(function(data) {
+		res.end(`${req.body.url} shortened to https://${config.domain}/${req.body.shortid ? req.body.shortid : random}`);
+	}).catch(function(e) {
+		res.status(500).end(`Unable to insert short URL into databse: ${e}`);
+		throw new Error(`Unable to insert short URL into databse: ${e}`);
+	});
+
+	/*var check = knex.select('url')
 		.from('links')
 		.where('id', random)
 		.toString();
@@ -127,7 +137,8 @@ app.post('/new', function (req, res) {
 				}
 			}
 		});
-	});
+	});*/
+
 });
 
 app.get('/onlinecheck', function (req, res) {
@@ -136,13 +147,94 @@ app.get('/onlinecheck', function (req, res) {
 	It will return a 200 instead of other codes which may 
 	result in an offline status in some monitoring programs
 	*/
-	res.status(200).write('OK');
+	res.status(200).end('OK');
 });
 
-app.get('*', function (req, res) {
-	res.status(404).json({ success: false, error: '404 Not Found' });
+app.get('/', function(req, res) {
+	res.redirect(config.homeRedirect);
 });
 
-var listener = app.listen(process.env.PORT, function () {
+app.get('/stats', function(req, res) {
+	res.sendFile(path.join(__dirname, 'stats.html'));
+});
+
+app.post('/stats', function(req, res) {
+	if (!(req.body.password || req.body.shortid)) {
+		return res.status(400).end('All fields are required');
+	}
+
+	if (req.body.password !== config.password) {
+		return res.status(403).end('Incorrect password');
+	}
+
+	if (req.body.shortid !== '*') {
+		knex('links').select('shortid', 'url', 'clicks').where({
+			shortid: req.body.shortid
+		}).then(function(data) {
+			if (data.length === 0) {
+				res.status(404).end('ID not found in database');
+			}
+
+			res.json(data[0]);
+		}).catch(function(e) {
+			res.status(500).end(`Error querying database: ${e}`);
+			throw new Error(`Error querying database: ${e}`);
+		});
+	} else {
+		knex('links').select('shortid', 'url', 'clicks').then(function(data) {
+			res.json(data);
+		}).catch(function(e) {
+			res.status(500).end(`Error querying database: ${e}`);
+			throw new Error(`Error querying database: ${e}`);
+		});
+	}
+
+});
+
+app.get('/:id', function (req, res) {
+	if (req.params.id === 'favicon.ico' || req.params.id === 'robots.txt') return res.status(404).end();
+	//console.log(req.params.id);
+	knex('links').select('shortid', 'url', 'clicks').where({
+		shortid: req.params.id
+	}).then(function(data) {
+		if (data.length === 0) {
+			return res.status(404).end('ID not found in database');
+		}
+
+		knex('links').update({
+			clicks: data[0].clicks + 1
+		}).where({
+			shortid: req.params.id
+		}).then(function() {
+			//res.end(data[0].url);
+			//res.json(data[0]);
+			res.redirect(data[0].url);
+		}).catch(function(e) {
+			res.status(500).end(`Failed to update click count: ${e}`);
+			throw new Error(`Failed to update click count: ${e}`);
+		});
+	}).catch(function(e) {
+		res.status(500).end(`Error querying database: ${e}`);
+		throw new Error(`Error querying database: ${e}`);
+	});
+
+	//console.log(selectURL);
+	/*db.serialize(function() {
+		db.all(selectURL, function (err, rows) {
+			if (err) {
+				console.log(err);
+			} else {
+				url = rows[0];
+				if (url) {
+					res.redirect(url.url);
+				} else {
+					res.status(400).json({ success: false, error: 'ID not found in database' });
+				}
+			}
+		});
+	});*/
+});
+
+var listener = app.listen(config.port, function () {
 	console.log(`App is listening on port ${listener.address().port}`);
 });
